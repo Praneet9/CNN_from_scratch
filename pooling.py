388770555e72985e188036fccbf980b62661d07c
Praneet9 <praneetbomma@gmail.com>
2020-08-16 17:27:43 +0530

    pooling layer done

--- pooling.py
+++ pooling.py
@@ -3,26 +3,29 @@ import numpy as np
 
 class Pooling():
 
-    def __init__(self, pooling_type='max', stride=2, kernel=3):
+    def __init__(self, input_shape, pooling_type='max', stride=2, kernel=2):
 
+        self.batch_size = input_shape[0]
+        self.input_height = input_shape[1]
+        self.input_width = input_shape[2]
+        self.input_channels = input_shape[3]
+        self.output_channels = input_shape[3]
         self.stride = stride
         self.pooling_type = pooling_type
+        self.output_height = int(1 + ((self.input_height - kernel) / stride))
+        self.output_width = int(1 + ((self.input_width - kernel) / stride))
         self.kernel = kernel
+        self.prev_act = np.zeros((self.batch_size, self.input_height, self.input_width, self.input_channels))
 
     def forward(self, prev_act):
+        prev_act = np.array(prev_act)
+        activation = np.zeros((self.batch_size, self.output_height, self.output_width, self.output_channels))
 
-        batch_size = prev_act.shape[0]
-        height = int(1 + (prev_act.shape[1] - self.kernel) / self.stride)
-        width = int(1 + (prev_act.shape[2] - self.kernel) / self.stride)
-        channels = prev_act.shape[3]
-
-        activation = np.zeros((batch_size, height, width, channels))
-
-        for m in batch_size:
+        for m in range(self.batch_size):
             image = prev_act[m]
-            for h in range(height):
-                for w in range(width):
-                    for c in range(channels):
+            for h in range(self.output_height):
+                for w in range(self.output_width):
+                    for c in range(self.output_channels):
                         vertical_start = h*self.stride
                         horizontal_start = w*self.stride
                         vertical_end = vertical_start + self.kernel
@@ -34,18 +37,17 @@ class Pooling():
                         elif self.pooling_type == 'avg':
                             activation[m, h, w, c] = np.mean(receptive_field)
 
-        return activation, prev_act
+        self.prev_act = prev_act.copy()
+        return activation
 
-    def backprop(self, dA, prev_act):
+    def backprop(self, dA):
+        prev_dA = np.zeros(self.prev_act.shape)
 
-        batch_size, height, width, channels = dA.shape
-        prev_dA = np.zeros(prev_act.shape)
-
-        for m in range(batch_size):
-            image = prev_act[m]
-            for h in range(height):
-                for w in range(width):
-                    for c in range(channels):
+        for m in range(self.batch_size):
+            image = self.prev_act[m]
+            for h in range(self.output_height):
+                for w in range(self.output_width):
+                    for c in range(self.output_channels):
                         vertical_start = h * self.stride
                         horizontal_start = w * self.stride
                         vertical_end = vertical_start + self.kernel
